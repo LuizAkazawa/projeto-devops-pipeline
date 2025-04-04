8be6f97da29a27b5a56b8ff26846b87766cb5208
Akazawa <luizakazawa@outlook.com>
2025-04-04 14:03:43 -0300

    change secrets yml

--- scripts/send_email.py
+++ scripts/send_email.py
@@ -2,36 +2,40 @@ import smtplib
 import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-#
+
 def send_email():
-    # Configurações Gmail
+    # Coletar variáveis de ambiente
+    sender_email = os.getenv("GMAIL_EMAIL")
+    password = os.getenv("GMAIL_PASSWORD")
+    receiver_email = os.getenv("DESTINATARIO_EMAIL")
+
+    if not sender_email or not password or not receiver_email:
+        raise ValueError("Uma ou mais variáveis de ambiente não estão definidas.")
+
     smtp_server = "smtp.gmail.com"
     port = 587
-    sender_email = os.environ["GMAIL_EMAIL"]
-    password = os.environ["GMAIL_PASSWORD"]
-    receiver_email = os.environ["DESTINATARIO_EMAIL"]
 
     # Criar mensagem
     msg = MIMEMultipart()
     msg["From"] = sender_email
     msg["To"] = receiver_email
     msg["Subject"] = "Notificação: Pipeline Iniciado"
-    
+
     body = """
     <p>O pipeline foi iniciado com sucesso!</p>
     <p><b>Atenção:</b> Este é um e-mail automático.</p>
     """
     msg.attach(MIMEText(body, "html"))
 
-    # Enviar
+    # Enviar e-mail
     try:
         with smtplib.SMTP(smtp_server, port) as server:
             server.starttls()
             server.login(sender_email, password)
             server.send_message(msg)
-        print("E-mail enviado!")
+        print("✅ E-mail enviado com sucesso.")
     except Exception as e:
-        print(f"Falha no envio: {e}")
+        print(f"❌ Erro ao enviar e-mail: {e}")
 
 if __name__ == "__main__":
     send_email()
